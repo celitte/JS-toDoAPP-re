b259f0bf9f247e618e021768f6c778647b2ce91f
celitte <kob221kt@gmail.com>
2023-06-10 14:06:34 +0900

    タスクの追加機能実装

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 
 const onClickAdd = () => {
-  alert();
-};
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
 
 
-document.getElementById("add-button")
-.addEventListener("click", () => onClickAdd());
+    const div = document.createElement("div");
+    div.className = "list-row";
+ 
+    const li = document.createElement("li");
+    li.innerText = inputText;
+
+
+    div.appendChild(li);
+
+    document.getElementById("incomplete-list").appendChild(div);
+  };
+
+
+// "add-button" というidを持つ要素を取得します
+const addButton = document.getElementById("add-button");
+
+// "click" イベントが発生した時に、onClickAdd() 関数が実行されるようにイベントリスナーを追加します
+addButton.addEventListener("click", () => {
+  onClickAdd();
+});
 
 
